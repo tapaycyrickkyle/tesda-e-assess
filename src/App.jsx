d31cf7111a2115727428514eab51e035f0baf9b5
Cyrick Kyle Tapay <tapaycyrick082@gmail.com>
2026-03-09 23:39:47 +0800

    added react

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <main className="min-h-screen grid place-items-center bg-zinc-950 text-white">
       <div className="rounded-2xl border border-white/10 bg-white/5 p-8 shadow-xl">
         <h1 className="text-3xl font-bold text-center">Welcome Team</h1>
-        <p className="mt-2 text-white">Nice nice nice.</p>
+        <p className="mt-2 text-white">Nice.</p>
         <div className="w-full h-auto flex justify-center items-center">
           <button className="mt-6 rounded-xl bg-white px-4 py-2 font-medium text-zinc-900 cursor-pointer">
             Button Desu
